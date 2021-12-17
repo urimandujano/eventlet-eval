3f34d10fe1f53cfb404ac91c0730a68d7bc8291e
Uriel Mandujano <uriel@globus.org>
2021-12-17 16:21:12 -0600

    Fixes eventlet usage
    - evenlet wasn't behaving correctly on python3.9

--- eventlet_eval/main.py
+++ eventlet_eval/main.py
@@ -1,17 +1,18 @@
 """
-This file contains makes an AWS API call to list ECS clusters 10 times. If run
-with an argument, the calls will be made using eventlet patching. Otherwise,
-calls will be made without any greening. Test case and timed results will be
-printed to stdout. 
+This file contains makes an AWS API call to list ECS clusters N_CALLS times. If
+run with an argument, the calls will be made using eventlet monkey-patching.
+Otherwise, calls will be made without any greening. Test case and timed results
+will be printed to stdout. 
 """
 
 import sys
 
-use_eventlet = len(sys.argv) > 1
-if use_eventlet:
+if use_eventlet := len(sys.argv) > 1:
     import eventlet
 
     eventlet.monkey_patch()
+
+    pool = eventlet.greenpool.GreenPool()
 
 import time
 from contextlib import contextmanager
@@ -33,9 +34,7 @@ def timeblock(label):
 
 
 def ecs_region(region_name):
-    cfg = Config(
-        region_name=region_name,
-    )
+    cfg = Config(region_name=region_name)
     ecs = boto3.client("ecs", config=cfg)
     try:
         result = ecs.list_clusters(maxResults=100)
@@ -45,6 +44,8 @@ def ecs_region(region_name):
     else:
         n_clusters = len(result["clusterArns"])
 
+    assert "clusterArns" in result, result.keys()
+    assert "ResponseMetadata" in result, result.keys()
     return f"{region_name}\t{result}\t{n_clusters}"
 
 
@@ -52,4 +53,10 @@ label = "with evenlet" if use_eventlet else "without eventlet"
 
 with timeblock(label):
     for _ in range(N_CALLS):
-        ecs_region("us-east-1")
+        if use_eventlet:
+            pool.spawn(ecs_region, "us-east-1")
+        else:
+            ecs_region("us-east-1")
+
+    if use_eventlet:
+        pool.waitall()
